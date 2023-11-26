06879f3cfb30f7644090be2ce345f5ffa1022fdd
BhanuBurman <bhanuburman@gmail.com>
2023-11-26 14:47:38 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); // This resolves the error
+
+// We will get an error since we cannot use environment variables directly in the backend
+// so we need to install dotenv using 'npm i dotenv' and import it manually
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(()=>{
+    console.log("Connected to MongDB");
+}).catch((error)=>{
+    console.log(error);
+});
+
+const app = express();
+app.listen(3000,()=>{
+    console.log('Server listening on port: 3000');
+})
