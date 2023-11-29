37c80ef6a97d0bbaddea3a42dafd863faa667df5
BhanuBurman <bhanuburman@gmail.com>
2023-11-29 23:24:10 +0530

    created signIn backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from 'jsonwebtoken';
 
 export const signUp =  async (req,res,next)=>{
     const {username,email,password} = req.body;
@@ -17,4 +17,29 @@ export const signUp =  async (req,res,next)=>{
         // next(errorHandler(550,"Error from tne function"));
     }
 
+}
+
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+    console.log(email, password);
+    try{
+        const validUser = await User.findOne({email: email});
+        if(!validUser){
+            return next(errorHandler(404,"User not found!"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(401,"Wrong credentials!"));
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_TOKEN_PASSWORD);
+        const {password:pass,...rest} = validUser._doc;
+
+
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200).json(rest)
+
+    } catch(err){
+        next(err);
+    }
 }
