6707ce04973cc10baf788121cbe3f7f8eba437c5
BhanuBurman <bhanuburman@gmail.com>
2023-11-29 18:51:14 +0530

    created manual errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 
-export const signUp =  async (req,res)=>{
+export const signUp =  async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
@@ -12,8 +13,8 @@ export const signUp =  async (req,res)=>{
         await newUser.save();
         res.status(201).json("User created successfully");
     }catch(err){
-        console.log(err.message);
-        res.status(500).json(err.message);
+        next(err);
+        // next(errorHandler(550,"Error from tne function"));
     }
 
 }
